ebae837944700eb388c1a89b646d02b6aaf600fd
linlaz <lazuardilintang@apps.ipb.ac.id>
2021-12-03 17:45:53 +0700

    everything about user

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\blogController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\userController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,12 +20,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'dashboard', 'middleware' => ['auth:sanctum']], function () {
+Route::group(['prefix' => 'dashboard', 'role_or_permission::admin|dashboard-menu'], function () {
     Route::get('/', [MenuController::class, 'index'])->name('dashboard');
 });
-Route::group(['prefix' => 'blogs', 'middleware' => ['auth:sanctum']], function () {
+Route::group(['prefix' => 'blogs', 'middleware' => ['auth:sanctum', 'role_or_permission::admin|dashboard-blog']], function () {
     Route::get('/', [blogController::class, 'index'])->name('blogs');
-    Route::get('/createblog',[blogController::class, 'create'])->name('createblog');
+    Route::get('/createblog', [blogController::class, 'create'])->name('createblog');
     Route::post('/createblogs', [blogController::class, 'store'])->name('storeblog');
     Route::get('/{slug}/edit', [blogController::class, 'edit'])->name('editblog');
     Route::post('/{blog}/edit', [blogController::class, 'update'])->name('updateblog');
@@ -32,6 +33,8 @@ Route::group(['prefix' => 'blogs', 'middleware' => ['auth:sanctum']], function (
 
 Route::get('/role', function () {
     return view('role.indexcontroller');
-})->name('role');
+})->name('role')->middleware('auth:sanctum', 'role_or_permission::admin|dashboard-role');
 
-
+Route::group(['prefix' => 'user', 'middleware' => ['auth:sanctum', 'role_or_permission::admin|dashboard-user']], function () {
+    Route::get('/', [userController::class, 'index'])->name('user');
+});
